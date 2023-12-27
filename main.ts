2ac8db0ce0a3377fc50aa5b97a0890554216f673
fct-droid <155027051+fct-droid@users.noreply.github.com>
2023-12-27 23:55:46 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,97 @@
+input.onButtonPressed(Button.A, function () {
+    music.setVolume(50)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.White)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Pinkish)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+    music.rest(music.beat(BeatFraction.Half))
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.White)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Pinkish)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+    music.rest(music.beat(BeatFraction.Whole))
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.White)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Pinkish)
+    music.play(music.tonePlayable(349, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+    music.rest(music.beat(BeatFraction.Whole))
+    Tinybit.RGB_Car_Big(Tinybit.enColor.White)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Cyan)
+    music.play(music.tonePlayable(196, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Pinkish)
+    music.play(music.tonePlayable(220, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    music.play(music.tonePlayable(247, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Green)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.Blue)
+    basic.pause(2000)
+    Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+})
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            . . # . .
+            . # # # .
+            # . # . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
